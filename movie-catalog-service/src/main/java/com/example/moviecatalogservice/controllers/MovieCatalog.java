dc6d14fa53f4208f74a145491b48eb1e56b39e38
AARSHI <aarora0301@gmail.com>
2019-06-25 23:51:09 +0800

    3.movie-catalog-service 3.1 Create model 'CatalogItem'for each movie bookmarked by user. 3.2 Create controller to get list of all movies rated by user. 3.3 Configure service to run on port 8090.

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/MovieCatalog.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/MovieCatalog.java
@@ -1,4 +1,18 @@
 package com.example.moviecatalogservice.controllers;
 
+import com.example.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalog {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(new CatalogItem("Transformers", "Thriller", 4));
+    }
 }
