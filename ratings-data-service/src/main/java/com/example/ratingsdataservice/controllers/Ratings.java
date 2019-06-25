6407552cbee7e3b114386fd8fae8271589784da5
AARSHI <aarora0301@gmail.com>
2019-06-26 00:31:39 +0800

    4.ratings-data-service 4.1 Create model 'Rating' to capture movie id and rating. 4.2 Create controller to get movie ratings. 4.3 Configure service to run on port 8092

--- ratings-data-service/src/main/java/com/example/ratingsdataservice/controllers/Ratings.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/controllers/Ratings.java
@@ -1,4 +1,16 @@
 package com.example.ratingsdataservice.controllers;
 
+import com.example.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
 public class Ratings {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
 }
