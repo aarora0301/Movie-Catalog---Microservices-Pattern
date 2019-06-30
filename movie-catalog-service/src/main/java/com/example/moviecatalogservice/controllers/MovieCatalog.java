28f975bd985ce2f06204e69bfb87e065526cdd42
AARSHI <aarora0301@gmail.com>
2019-07-01 02:52:36 +0800

    15. Implement log tracing by adding unique id's to logs: Sleuth 15.1 Add spring sleuth as dependency. 15.2 Add logger in catalog-service that will print trace-id and span-id      of each request.

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/MovieCatalog.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/controllers/MovieCatalog.java
@@ -2,9 +2,9 @@ package com.example.moviecatalogservice.controllers;
 
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
-import com.example.moviecatalogservice.models.Rating;
 import com.example.moviecatalogservice.models.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalog {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieCatalog.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -27,7 +29,7 @@ public class MovieCatalog {
     @HystrixCommand(fallbackMethod = "reliable")
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
+        LOGGER.info("Fetching catalogs bookmarked by user ... ");
 
         //Task 1. get all rated movie ids
         UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
